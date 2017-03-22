e4cf91da1464c851ae787fcea1ff576e8746aeb2
Erik Enderlein <erik.end@gmail.com>
2017-03-22 18:45:22 -0500

    more templating work. Aded dice colors

--- app.js
+++ app.js
@@ -7,28 +7,36 @@ function wizCard(data){
   this.id = data.id;
   this.effectText = data.effectText;
   this.dice = [];
+  this.colors = [];
   for (var i = 0; i < data.dice.length; i++) {
     switch (data.dice[i]) {
     case 'redFocus':
-      this.dice.push('icons/blank.jpg','icons/leaf.png','icons/water.png','icons/fire.jpg','icons/fire.jpg','icons/doubleFire.png');
+      this.dice.push('icons/blank.png','icons/leaf.png','icons/water.png','icons/fire.png','icons/fire.png','icons/doubleFire.png');
+      this.colors.push('red');
       break;
     case 'blueFocus':
-      this.dice.push('icons/blank.jpg','icons/fire.jpg','icons/leaf.png','icons/water.png','icons/water.png','icons/doubleWater.png');
+      this.dice.push('icons/blank.png','icons/fire.png','icons/leaf.png','icons/water.png','icons/water.png','icons/doubleWater.png');
+      this.colors.push('blue');
       break;
     case 'greenFocus':
-      this.dice.push('icons/blank.jpg','icons/fire.jpg','icons/water.png','icons/leaf.png','icons/leaf.png','icons/doubleLeaf.png');
+      this.dice.push('icons/blank.png','icons/fire.png','icons/water.png','icons/leaf.png','icons/leaf.png','icons/doubleLeaf.png');
+      this.colors.push('green');
       break;
     case 'redGreenHybrid':
-      this.dice.push('icons/blank.jpg','icons/fire.jpg','icons/fire.jpg','icons/leaf.png','icons/leaf.png','icons/fireLeaf.jpg');
+      this.dice.push('icons/blank.png','icons/fire.png','icons/fire.png','icons/leaf.png','icons/leaf.png','icons/fireLeaf.png');
+      this.colors.push('yellow');
       break;
     case 'redBlueHybrid':
-      this.dice.push('icons/blank.jpg','icons/fire.jpg','icons/fire.jpg','icons/water.png','icons/water.png','icons/fireWater.jpg');
+      this.dice.push('icons/blank.png','icons/fire.png','icons/fire.png','icons/water.png','icons/water.png','icons/fireWater.png');
+      this.colors.push('purple');
       break;
     case 'greenBlueHybrid':
-      this.dice.push('icons/blank.jpg','icons/leaf.png','icons/leaf.png','icons/water.png','icons/water.png','icons/leafWater.png');
+      this.dice.push('icons/blank.png','icons/leaf.png','icons/leaf.png','icons/water.png','icons/water.png','icons/leafWater.png');
+      this.colors.push('cyan');
       break;
     case 'chaos':
-      this.dice.push('icons/blank.jpg','icons/blank.jpg','icons/fire.jpg','icons/leaf.png','icons/water.png','icons/rainbowpower.jpg');
+      this.dice.push('icons/blank.png','icons/blank.png','icons/fire.png','icons/leaf.png','icons/water.png','icons/rainbowpower.png');
+      this.colors.push('white');
       break;
     }
   }
@@ -56,6 +64,7 @@ wizObjectArray.forEach(function(i) {
       $(`#${i.id} .diceOne`).append($('<img/>')
       .addClass('square')
       .attr('src',j)
+      .attr('style',`background-color: ${i.colors[0]}`)
       );
       $(`#${i.id} .diceOne`).append($('<img/>')
       .addClass('diceIcon')
@@ -65,6 +74,7 @@ wizObjectArray.forEach(function(i) {
       $(`#${i.id} .diceTwo`).append($('<img/>')
       .addClass('square')
       .attr('src',j)
+      .attr('style',`background-color: ${i.colors[1]}`)
       );
       $(`#${i.id} .diceTwo`).append($('<img/>')
       .addClass('diceIcon')
@@ -74,6 +84,7 @@ wizObjectArray.forEach(function(i) {
       $(`#${i.id} .diceThree`).append($('<img/>')
       .addClass('square')
       .attr('src',j)
+      .attr('style',`background-color: ${i.colors[2]}`)
       );
       $(`#${i.id} .diceThree`).append($('<img/>')
       .addClass('diceIcon')
@@ -82,4 +93,4 @@ wizObjectArray.forEach(function(i) {
   }
   });
 });
-//code heavily drawn from http://stackoverflow.com/questions/10619445/the-prefeicons/fire.jpg-way-of-creating-a-new-element-with-jquery
+//code heavily drawn from http://stackoverflow.com/questions/10619445/the-prefeicons/fire.png-way-of-creating-a-new-element-with-jquery
