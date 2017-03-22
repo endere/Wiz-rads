dffe120a56e9a0553f24a01ad33cfe6c541a7284
Erik Enderlein <erik.end@gmail.com>
2017-03-22 14:46:26 -0500

    made wizard templates

---
+++ rawData.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var rawData = [
+  {
+    name: 'Phormonius the Arcane',
+    id: 'phormonius',
+    img: 'wizards/f002de5d3c8067756a6b2a0c66470f13.jpg',
+    dice: ['redBlueHybrid','blueFocus','greenBlueHybrid'],
+    effectText: 'Placeholder text'
+  },
+  {
+    name: 'Mickus Trickus',
+    id: 'mick',
+    img: 'wizards/how-to-draw-fantasia-wizard-mickey_1_000000008546_5.jpg',
+    dice: ['blueFocus','greenBlueHybrid','greenFocus'],
+    effectText: 'Lord of brooms.'
+  },
+  {
+    name: 'Knott Asham',
+    id: 'knott',
+    img: 'wizards/magicians-collection-002-282x400.jpg',
+    dice: ['redBlueHybrid','redGreenHybrid','greenBlueHybrid'],
+    effectText: 'He totally knows magic for real.'
+  },
+];
